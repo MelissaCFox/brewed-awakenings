f947c5f0016b4f6667d5d12074587a4262fcea96
MelissaCFox <melissa.caitlin.fox@gmail.com>
2021-10-13 15:10:35 -0500

    completed all brewed awakening chapters within book 3

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,32 @@
 import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    let numberOfProductsSold = 0
+                    for (const order of orders) {
+                        if (order.employeeId === employee.id) {
+                            numberOfProductsSold ++
+                        }
+                    } 
+                    window.alert(`${employee.name} sold ${numberOfProductsSold} products.`)
+                }
+            }
+        }
+    }
+)
+
+
 
 export const Employees = () => {
     let html = "<ul>"
